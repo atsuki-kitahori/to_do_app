3ee1c013867bf04b37368cd4fb01ec1df6d9b229
klum-kan <70319590+klum-kan@users.noreply.github.com>
2020-10-19 17:56:52 +0900

    Vue.js導入

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   resources :to_dos
   root 'to_dos#index'
+  namespace :api, { format: 'json' } do
+    namespace :v1 do
+      resources :to_dos
+    end
+  end
 end
