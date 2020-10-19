6ef5df6c6f41df1fabeeee308de211cfa43ba425
klum-kan <70319590+klum-kan@users.noreply.github.com>
2020-10-19 15:47:27 +0900

    期限表示

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_19_020703) do
+ActiveRecord::Schema.define(version: 2020_10_19_064129) do
 
   create_table "to_dos", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title", null: false
     t.boolean "finished", default: false, null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.datetime "expired_at"
   end
 
 end
