65a86e91bc02adf28e294be02354ca39ec8725b1
klum-kan <70319590+klum-kan@users.noreply.github.com>
2020-10-22 14:03:58 +0900

    vueの反映の修正

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -5,4 +5,4 @@ const vue = require('./loaders/vue')
 environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
 environment.loaders.prepend('vue', vue)
 module.exports = environment
-environment.loaders.get('vue').options.extractCSS = false
+environment.loaders.get('vue').use[0].options.extractCSS = false
