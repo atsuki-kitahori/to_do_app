982facc340f4112f8c4108fa5c744e8c7c178a02
klum-kan <70319590+klum-kan@users.noreply.github.com>
2020-10-22 14:06:58 +0900

    Revert "vueの反映"
    This reverts commit 91b581e3e5b3ecb49dad5fde7a440265982675a5.

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -5,4 +5,3 @@ const vue = require('./loaders/vue')
 environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
 environment.loaders.prepend('vue', vue)
 module.exports = environment
-environment.loaders.get('vue').options.extractCSS = false
